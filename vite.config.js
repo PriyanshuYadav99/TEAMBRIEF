61e62fac68eabe3135e07215cc8fb8fb6705c31a
PriyanshuYadav99 <9670111545ak@gmail.com>
2025-11-10 16:04:37 +0530

    fix: update index.html for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
   build: {
-    outDir: 'dist', // ensure Vite builds to the "dist" folder
-  },
+    outDir: 'dist'
+  }
 })
